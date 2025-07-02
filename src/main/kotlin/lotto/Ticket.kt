9438180a5ced411c1ef6c025268fb1074e1a00a8
AzureAD\ApoorvaSomanahallyNa <apoorva1326@gmail.com>
2025-07-02 12:07:02 +0200

    test: add ticket numbers test

--- src/main/kotlin/lotto/Ticket.kt
+++ src/main/kotlin/lotto/Ticket.kt
@@ -1,6 +1,6 @@
 package lotto
 
-class Ticket(numbers:List<Int>) {
+class Ticket(val numbers:List<Int>) {
     init {
         require(numbers.size == 6 && numbers.distinct().size == 6) {"Numbers have to be distinct"}
         require(numbers.all{it in 1..45 }) {"Numbers should be between 1 to 45"}
--- src/test/kotlin/lotto/TicketTest.kt
+++ src/test/kotlin/lotto/TicketTest.kt
@@ -1,14 +1,33 @@
 package lotto
 
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
-import kotlin.test.Test
+
 
 class TicketTest {
+
+    @Test
+    fun `size ticket numbers are not 6` () {
+        assertThrows<IllegalArgumentException> {
+            val ticket = Ticket(listOf(10,2,34,5,2,15,6))
+            require(ticket.numbers.size == 6)
+        }
+    }
+
+    @Test
+    fun `size ticket numbers is 6`() {
+        assertDoesNotThrow {
+            val ticket = Ticket(listOf(1,2,34,6,45,41))
+            require(ticket.numbers.size == 6)
+        }
+    }
+
     @Test
     fun `ticket numbers are between 1 and 45`(){
         assertDoesNotThrow {
             val ticket = Ticket(listOf(1,2,34,6,45,41))
+            require(ticket.numbers.all{it in 1..45})
         }
     }
 
@@ -16,6 +35,7 @@ class TicketTest {
     fun `ticket numbers are not between 1 and 45`(){
         assertThrows<IllegalArgumentException> {
             val ticket = Ticket(listOf(1,2,34,46,4,41))
+            require(ticket.numbers.all{it in 1..45})
         }
     }
 
@@ -23,13 +43,15 @@ class TicketTest {
     fun `ticket numbers are not duplicate`(){
         assertThrows<IllegalArgumentException> {
             val ticket = Ticket(listOf(1,1,2,3,4,5))
+            require(ticket.numbers.size == ticket.numbers.distinct().size)
         }
     }
 
     @Test
     fun `ticket numbers should not be empty`(){
         assertThrows<IllegalArgumentException> {
-            Ticket(emptyList())
+            val ticket = Ticket(emptyList())
+            require(ticket.numbers.size >= 0)
         }
     }
 }
