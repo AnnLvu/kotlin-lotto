b3e504c4e8ea54dd3bb6c3e323c29b3599990ee9
AzureAD\AnastasiiaLvutina <anasteishanakir@gmail.com>
2025-07-05 17:20:50 +0200

    test(lotto): edit tests after restructure project

--- src/test/kotlin/lotto/TicketTest.kt
+++ src/test/kotlin/lotto/TicketTest.kt
@@ -1,8 +1,8 @@
 package lotto
 
-import lotto.controller.*
-import lotto.model.*
-import lotto.view.*
+import lotto.model.Const
+import lotto.model.Numbers
+import lotto.model.Ticket
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
@@ -11,56 +11,56 @@ class TicketTest {
     @Test
     fun `size ticket numbers are not 6`() {
         assertThrows<IllegalArgumentException> {
-            val ticket = Ticket(listOf(10, 2, 34, 5, 2, 15, 6))
-            require(ticket.numbers.size == Const.NUMBER_COUNT)
+            val ticket = Ticket(Numbers(listOf(10, 2, 34, 5, 2, 15, 6)))
+            require(ticket.numbers.getNumbers().size == Const.NUMBER_COUNT)
         }
     }
 
     @Test
     fun `size ticket numbers is 6`() {
         assertDoesNotThrow {
-            val ticket = Ticket(listOf(1, 2, 34, 6, 45, 41))
-            require(ticket.numbers.size == Const.NUMBER_COUNT)
+            val ticket = Ticket(Numbers(listOf(1, 2, 34, 6, 45, 41)))
+            require(ticket.numbers.getNumbers().size == Const.NUMBER_COUNT)
         }
     }
 
     @Test
     fun `ticket numbers are between 1 and 45`() {
         assertDoesNotThrow {
-            val ticket = Ticket(listOf(1, 2, 34, 6, 45, 41))
-            require(ticket.numbers.all { it in Const.MIN..Const.MAX })
+            val ticket = Ticket(Numbers(listOf(1, 2, 34, 6, 45, 41)))
+            require(ticket.numbers.getNumbers().all { it in Const.MIN..Const.MAX })
         }
     }
 
     @Test
     fun `ticket numbers are not between 1 and 45`() {
         assertThrows<IllegalArgumentException> {
-            val ticket = Ticket(listOf(1, 2, 34, 46, 4, 41))
-            require(ticket.numbers.all { it in Const.MIN..Const.MAX })
+            val ticket = Ticket(Numbers(listOf(1, 2, 34, 46, 4, 41)))
+            require(ticket.numbers.getNumbers().all { it in Const.MIN..Const.MAX })
         }
     }
 
     @Test
     fun `ticket numbers are duplicate`() {
         assertThrows<IllegalArgumentException> {
-            val ticket = Ticket(listOf(1, 1, 2, 3, 4, 5))
-            require(ticket.numbers.size == ticket.numbers.distinct().size)
+            val ticket = Ticket(Numbers(listOf(1, 1, 2, 3, 4, 5)))
+            require(ticket.numbers.getNumbers().size == ticket.numbers.getNumbers().distinct().size)
         }
     }
 
     @Test
     fun `ticket numbers are not duplicate`() {
         assertDoesNotThrow {
-            val ticket = Ticket(listOf(1, 10, 2, 3, 4, 5))
-            require(ticket.numbers.size == ticket.numbers.distinct().size)
+            val ticket = Ticket(Numbers(listOf(1, 10, 2, 3, 4, 5)))
+            require(ticket.numbers.getNumbers().size == ticket.numbers.getNumbers().distinct().size)
         }
     }
 
     @Test
     fun `ticket numbers should not be empty`() {
         assertThrows<IllegalArgumentException> {
-            val ticket = Ticket(emptyList())
-            require(ticket.numbers.size >= 0)
+            val ticket = Ticket(Numbers(emptyList()))
+            require(ticket.numbers.getNumbers().size >= 0)
         }
     }
 }
