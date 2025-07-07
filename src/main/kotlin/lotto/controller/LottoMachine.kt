31caba740959392b32b2ffcae7eec03ff286c10a
AzureAD\AnastasiiaLvutina <anasteishanakir@gmail.com>
2025-07-07 15:23:01 +0200

    refactor(controller): extract purchaseTickets() to private function

--- src/main/kotlin/lotto/controller/LottoMachine.kt
+++ src/main/kotlin/lotto/controller/LottoMachine.kt
@@ -7,17 +7,23 @@ import lotto.view.InputView
 import lotto.view.OutputView
 
 object LottoMachine {
+
     fun start() {
-        val amount = InputView.inputPurchaseAmount()
-        val maxTickets = amount / Const.PRICE
-        val manualTicketCount = InputView.inputManualTicketCount(maxTickets)
-        val manualTickets = InputView.inputManualTickets(manualTicketCount)
-        val tickets = Tickets.generate(amount, manualTickets)
+        val (tickets, manualTicketCount, amount) = purchaseTickets()
         OutputView.displayTickets(tickets, manualTicketCount)
         val winningNumbers = InputView.inputWinningNumbers()
         val bonusNumber = InputView.inputBonusNumber(winningNumbers)
         val winningTicket = WinningTicket(winningNumbers, bonusNumber)
         val winStats = tickets.calculateStats(winningTicket)
         OutputView.displayResults(winStats, tickets.calculateReturnRate(amount, winStats))
+    }
+
+    private fun purchaseTickets(): Triple<Tickets, Int, Int> {
+        val amount = InputView.inputPurchaseAmount()
+        val maxTickets = amount / Const.PRICE
+        val manualTicketCount = InputView.inputManualTicketCount(maxTickets)
+        val manualTickets = InputView.inputManualTickets(manualTicketCount)
+        val tickets = Tickets.generate(amount, manualTickets)
+        return Triple(tickets, manualTicketCount, amount)
     }
 }
